f9f3c83e66d613c9f970092c2caa748142c0b45e
Shalinichandran05 <shalinickr05@gmail.com>
2026-06-03 11:48:53 +0530

    deleted api folder supabase

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-// Vercel Native Serverless Route wrapper
-// This allows zero-conf routing on Vercel for the Express app
-
-const app = require('../backend/server.js');
-module.exports = app;
